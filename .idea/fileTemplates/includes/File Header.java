7e31c5e88dd26dc2af5ec6cf41695c455aef6a8c
Bloogefest <bloogefest@gmail.com>
2022-10-23 16:22:32 +0300

    Added IMutableSetter and IMutableAccessor

--- .idea/fileTemplates/includes/File Header.java
+++ .idea/fileTemplates/includes/File Header.java
@@ -1,6 +1,6 @@
 /**
 * @apiNote Not specified
 * @author Bloogefest
-* @since 1.0.9
+* @since 1.1.0
 * @version 1.0
 */
---
+++ src/dev/common/function/IMutableAccessor.java
@@ -0,0 +1,41 @@
+package dev.common.function;
+
+import dev.common.base.MutateException;
+
+/**
+ * @author Bloogefest
+ * @version 1.0
+ * @apiNote Not specified
+ * @since 1.0.9
+ */
+public interface IMutableAccessor<T> extends IAccessor<T>, IMutableSetter<T> {
+
+    /**
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.0.9
+     */
+    @Override
+    T get();
+
+    /**
+     * @param value Not specified
+     * @throws MutateException Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.0.9
+     */
+    @Override
+    void set(final T value) throws MutateException;
+
+    /**
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.0.9
+     */
+    @Override
+    boolean mutable();
+
+}
