d38fce76786fafe225bf90213ee871783ae5f512
Bloogefest <bloogefest@gmail.com>
2022-10-25 01:28:17 +0300

    Improve numbers API

--- src/dev/common/general/Numbers.java
+++ src/dev/common/general/Numbers.java
@@ -22,4 +22,160 @@ public final class Numbers {
         throw new SecurityException();
     }
 
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean equals(final byte value, final byte value_) {
+        return value == value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean equals(final short value, final short value_) {
+        return value == value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean equals(final int value, final int value_) {
+        return value == value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean equals(final long value, final long value_) {
+        return value == value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean equals(final float value, final float value_) {
+        return value == value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean equals(final double value, final double value_) {
+        return value == value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean notEquals(final byte value, final byte value_) {
+        return value != value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean notEquals(final short value, final short value_) {
+        return value != value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean notEquals(final int value, final int value_) {
+        return value != value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean notEquals(final long value, final long value_) {
+        return value != value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean notEquals(final float value, final float value_) {
+        return value != value_;
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> _")
+    public static boolean notEquals(final double value, final double value_) {
+        return value != value_;
+    }
+
 }
