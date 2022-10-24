7de43119d55946410191e1534991469d1227c489
Bloogefest <bloogefest@gmail.com>
2022-10-25 01:39:41 +0300

    Overload Validator#equals, Validator#notEquals for primitives

--- src/dev/common/validation/Validator.java
+++ src/dev/common/validation/Validator.java
@@ -1,5 +1,6 @@
 package dev.common.validation;
 
+import dev.common.general.Numbers;
 import dev.common.throwable.SoftException;
 import dev.common.function.ISupplier;
 import dev.common.general.Objects;
@@ -73,17 +74,123 @@ public final class Validator {
     }
 
     /**
-     * @param <OBJECT> Not specified
-     * @param object The source object
-     * @param object_ The subsequent object
-     * @return The source object
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
      * @throws NotEqualsException The exception
      * @author Bloogefest
      * @apiNote Not specified
      * @since 1.1.3
      */
     @Contract("_, _ -> param1")
-    public static <OBJECT> OBJECT equals(final OBJECT object, final OBJECT object_) throws NotEqualsException {
+    public static boolean equals(final boolean value, final boolean value_) throws NotEqualsException {
+        if (Objects.equals(value, value_)) return value;
+        throw new NotEqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws NotEqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static byte equals(final byte value, final byte value_) throws NotEqualsException {
+        if (Numbers.equals(value, value_)) return value;
+        throw new NotEqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws NotEqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static short equals(final short value, final short value_) throws NotEqualsException {
+        if (Numbers.equals(value, value_)) return value;
+        throw new NotEqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws NotEqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static int equals(final int value, final int value_) throws NotEqualsException {
+        if (Numbers.equals(value, value_)) return value;
+        throw new NotEqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws NotEqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static long equals(final long value, final long value_) throws NotEqualsException {
+        if (Numbers.equals(value, value_)) return value;
+        throw new NotEqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws NotEqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static float equals(final float value, final float value_) throws NotEqualsException {
+        if (Numbers.equals(value, value_)) return value;
+        throw new NotEqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws NotEqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static double equals(final double value, final double value_) throws NotEqualsException {
+        if (Numbers.equals(value, value_)) return value;
+        throw new NotEqualsException();
+    }
+
+    /**
+     * @param <OBJECT> Not specified
+     * @param object The source object
+     * @param object_ The subsequent object
+     * @return The source object
+     * @throws NullException Not specified
+     * @throws NotEqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static <OBJECT> OBJECT equals(final OBJECT object, final OBJECT object_) throws NullException, NotEqualsException {
         if (Objects.equals(object, object_)) return object;
         throw new NotEqualsException();
     }
@@ -95,13 +202,14 @@ public final class Validator {
      * @param name The name of the source object
      * @param name_ The name of the subsequent object
      * @return The source object
+     * @throws NullException Not specified
      * @throws NotEqualsException The exception
      * @author Bloogefest
      * @apiNote Not specified
      * @since 1.1.3
      */
-    @Contract("_, _, _, _ -> param1")
-    public static <OBJECT> OBJECT equals(final OBJECT object, final OBJECT object_, final String name, final String name_) throws NotEqualsException {
+    @Contract("_, _, !null, !null -> param1; _, _, null, null -> fail")
+    public static <OBJECT> OBJECT equals(final OBJECT object, final OBJECT object_, final String name, final String name_) throws NullException, NotEqualsException {
         if (Objects.equals(object, object_)) return object;
         throw new NotEqualsException(NotEqualsException.LAYOUT_MESSAGE.formatted(notNull(name, "name of the source object"), notNull(name_, "name of the subsequent object")));
     }
@@ -112,21 +220,128 @@ public final class Validator {
      * @param object_ The subsequent object
      * @param supplier The custom exception provider
      * @return The source object
+     * @throws NullException Not specified
      * @throws THROWABLE The custom exception
      * @author Bloogefest
      * @apiNote Not specified
      * @since 1.1.3
      */
-    @Contract("_, _, _ -> param1")
-    public static <OBJECT, THROWABLE extends SoftException> OBJECT equals(final OBJECT object, final OBJECT object_, final ISupplier<THROWABLE> supplier) throws THROWABLE {
+    @Contract("_, _, !null -> param1; _, _, null -> fail")
+    public static <OBJECT, THROWABLE extends SoftException> OBJECT equals(final OBJECT object, final OBJECT object_, final ISupplier<THROWABLE> supplier) throws NullException, THROWABLE {
         if (Objects.equals(object, object_)) return object;
         throw notNull(notNull(supplier, "supplier").supply(), "throwable");
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws EqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static boolean notEquals(final boolean value, final boolean value_) throws EqualsException {
+        if (Objects.notEquals(value, value_)) return value;
+        throw new EqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws EqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static byte notEquals(final byte value, final byte value_) throws EqualsException {
+        if (Numbers.notEquals(value, value_)) return value;
+        throw new EqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws EqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static short notEquals(final short value, final short value_) throws EqualsException {
+        if (Numbers.notEquals(value, value_)) return value;
+        throw new EqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws EqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static int notEquals(final int value, final int value_) throws EqualsException {
+        if (Numbers.notEquals(value, value_)) return value;
+        throw new EqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws EqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static long notEquals(final long value, final long value_) throws EqualsException {
+        if (Numbers.notEquals(value, value_)) return value;
+        throw new EqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws EqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static float notEquals(final float value, final float value_) throws EqualsException {
+        if (Numbers.notEquals(value, value_)) return value;
+        throw new EqualsException();
+    }
+
+    /**
+     * @param value The source value
+     * @param value_ The subsequent value
+     * @return The source value
+     * @throws EqualsException The exception
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.3
+     */
+    @Contract("_, _ -> param1")
+    public static double notEquals(final double value, final double value_) throws EqualsException {
+        if (Numbers.notEquals(value, value_)) return value;
+        throw new EqualsException();
     }
 
     /**
      * @param <OBJECT> Not specified
      * @param object The source object
      * @param object_ The subsequent object
+     * @throws NullException Not specified
      * @throws EqualsException The exception
      * @return The source object
      * @author Bloogefest
@@ -134,7 +349,7 @@ public final class Validator {
      * @since 1.1.3
      */
     @Contract("_, _ -> param1")
-    public static <OBJECT> OBJECT notEquals(final OBJECT object, final OBJECT object_) throws EqualsException {
+    public static <OBJECT> OBJECT notEquals(final OBJECT object, final OBJECT object_) throws NullException, EqualsException {
         if (Objects.notEquals(object, object_)) return object;
         throw new EqualsException();
     }
@@ -145,14 +360,15 @@ public final class Validator {
      * @param object_ The subsequent object
      * @param name The name of the source object
      * @param name_ The name of the subsequent object
+     * @throws NullException Not specified
      * @throws EqualsException The exception
      * @return The source object
      * @author Bloogefest
      * @apiNote Not specified
      * @since 1.1.3
      */
-    @Contract("_, _, _, _ -> param1")
-    public static <OBJECT> OBJECT notEquals(final OBJECT object, final OBJECT object_, final String name, final String name_) throws EqualsException {
+    @Contract("_, _, !null, !null -> param1; _, _, null, null -> fail")
+    public static <OBJECT> OBJECT notEquals(final OBJECT object, final OBJECT object_, final String name, final String name_) throws NullException, EqualsException {
         if (Objects.notEquals(object, object_)) return object;
         throw new EqualsException(EqualsException.LAYOUT_MESSAGE.formatted(notNull(name, "name of the source object"), notNull(name_, "name of the subsequent object")));
     }
@@ -163,13 +379,14 @@ public final class Validator {
      * @param object_ The subsequent object
      * @param supplier The custom exception provider
      * @return The source object
+     * @throws NullException Not specified
      * @throws THROWABLE The custom exception
      * @author Bloogefest
      * @apiNote Not specified
      * @since 1.1.3
      */
-    @Contract("_, _, _ -> param1")
-    public static <OBJECT, THROWABLE extends SoftException> OBJECT notEquals(final OBJECT object, final OBJECT object_, final ISupplier<THROWABLE> supplier) throws THROWABLE {
+    @Contract("_, _, !null -> param1; _, _, null -> fail")
+    public static <OBJECT, THROWABLE extends SoftException> OBJECT notEquals(final OBJECT object, final OBJECT object_, final ISupplier<THROWABLE> supplier) throws NullException, THROWABLE {
         if (Objects.notEquals(object, object_)) return object;
         throw notNull(notNull(supplier, "supplier").supply(), "throwable");
     }
