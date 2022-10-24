727aed3c8b4a614c722a7543da94cceebb947414
Bloogefest <bloogefest@gmail.com>
2022-10-24 18:46:56 +0300

    Added utility for numbers

---
+++ src/dev/common/base/Numbers.java
@@ -0,0 +1,21 @@
+package dev.common.base;
+
+/**
+ * @author Bloogefest
+ * @version 1.0
+ * @apiNote Not specified
+ * @since 1.1.2
+ */
+public final class Numbers {
+
+    /**
+     * @throws CreationError Not specified
+     * @author Bloogefest
+     * @apiNote Not specified
+     * @since 1.1.2
+     */
+    private Numbers() throws CreationError {
+        throw new CreationError();
+    }
+
+}
